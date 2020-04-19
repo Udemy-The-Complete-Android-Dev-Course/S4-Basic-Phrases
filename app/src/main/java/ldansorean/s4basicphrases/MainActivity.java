ffbef5f71b477bcb19aec834b9a2908233853171
ldansorean <9722740+ldansorean@users.noreply.github.com>
2020-04-19 14:40:04 +0100

    Add buttons and audio files. Play corresponding audio file on button click.

--- app/src/main/java/ldansorean/s4basicphrases/MainActivity.java
+++ app/src/main/java/ldansorean/s4basicphrases/MainActivity.java
@@ -2,13 +2,23 @@ package ldansorean.s4basicphrases;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void phraseSelected(View view) {
+        int audioFileId = getResources().getIdentifier(view.getTag().toString(),"raw", getPackageName());
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, audioFileId);
+        mediaPlayer.start();
     }
 }
